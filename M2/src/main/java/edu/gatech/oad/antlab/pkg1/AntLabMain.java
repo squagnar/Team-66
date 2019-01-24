1996b4e71b3dfb8d216d7c03966e0eb1ba3e91ab
maryjiang <maryjiang@gatech.edu>
2019-01-24 12:30:06 -0500

    changed Person3

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.AntLab22;
 		  toPrint += p2.toString("gburdell2");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("Mary hi");
+		  Person3 p3 = new Person3("Mary");
 		  toPrint += p3.toString("mjiang48");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
