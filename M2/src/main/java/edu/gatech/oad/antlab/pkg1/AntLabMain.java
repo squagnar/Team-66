650403b71b1707a32e86e353dea30d2d49cc0d25
bragalotwill <wmbraga7@gmail.com>
2019-01-24 12:27:44 -0500

    Replaced Person4 Strings

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -3,7 +3,8 @@ package edu.gatech.oad.antlab.pkg1;
 
 import edu.cs2335.antlab.pkg3.*;
 import edu.gatech.oad.antlab.person.*;
-import edu.gatech.oad.antlab.pkg2.*;
+import edu.gatech.oad.antlab.pkg2.AntLab21;
+import edu.gatech.oad.antlab.pkg2.AntLab22;
 
 
 /**
@@ -69,8 +70,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("gburdell3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+        Person4 p4 = new Person4("William");
+        toPrint += p4.toString("wbraga3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("P5");
