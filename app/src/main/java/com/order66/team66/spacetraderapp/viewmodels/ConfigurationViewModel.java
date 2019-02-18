3376b866111190951425474028e2f44410317e9d
maryjiang <maryjiang@gatech.edu>
2019-02-18 14:31:23 -0500

    scaled down attributes in content_configuration, edited ConfigurationActivity

--- app/src/main/java/com/order66/team66/spacetraderapp/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/com/order66/team66/spacetraderapp/viewmodels/ConfigurationViewModel.java
@@ -5,7 +5,12 @@ import com.order66.team66.spacetraderapp.models.Game;
 import com.order66.team66.spacetraderapp.models.Player;
 
 public class ConfigurationViewModel extends ViewModel {
-
+    private String name;
+    // private Button difficultyField;
+    private int pointsFighter;
+    private int pointsTrader;
+    private int pointsPilot;
+    private int pointsEngineer;
     /**
      * Passthrough method to allow a view to create a Player object
      *
@@ -18,5 +23,25 @@ public class ConfigurationViewModel extends ViewModel {
     public void createPlayer(String name, int pilot, int fighter, int trader, int engineer) {
         Player player = new Player(name, pilot, fighter, trader, engineer);
         //TODO: store this somewhere, maybe firebase database
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPilot(int points) {
+        this.pointsPilot = points;
+    }
+
+    public void setPointsFighter(int points) {
+        this.pointsFighter = points;
+    }
+
+    public void setPointsTrader(int points) {
+        this.pointsTrader = points;
+    }
+
+    public void setPointsEngineer(int points) {
+        this.pointsEngineer = points;
     }
 }
--- app/src/main/java/com/order66/team66/spacetraderapp/views/ConfigurationActivity.java
+++ app/src/main/java/com/order66/team66/spacetraderapp/views/ConfigurationActivity.java
@@ -7,9 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 import com.order66.team66.spacetraderapp.R;
 import com.order66.team66.spacetraderapp.models.Difficulty;
 import com.order66.team66.spacetraderapp.models.Player;
@@ -23,35 +21,24 @@ public class ConfigurationActivity extends AppCompatActivity {
 
     // Widgets to get info
     private EditText nameField;
-    // private Button difficultyField;
     private TextView pointsFighter;
     private TextView pointsTrader;
     private TextView pointsPilot;
     private TextView pointsEngineer;
 
-    // point retrieval for point system
-    int pointVal = 0;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button button;
 
-    public void increasePoint(View view) {
-        pointVal++;
-        // display(pointVal);
-    }
-
-    public void decreasePoint(View view) {
-        pointVal--;
-        // display(pointVal);
-    }
-    public void display(int value, TextView id) {
-        TextView pointDisplay = (TextView) findViewById(id.getId());
-        pointDisplay.setText("" + value);
-    }
+    int pointValPilot = 0;
+    int pointValFighter = 0;
+    int pointValTrader = 0;
+    int pointValEngineer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_configuration);
-
-        int pointVal = 0;
 
         // widgets
         nameField = findViewById(R.id.player_name_input);
@@ -60,16 +47,88 @@ public class ConfigurationActivity extends AppCompatActivity {
         pointsTrader = findViewById(R.id.trader_points);
         pointsEngineer = findViewById(R.id.engineer_points);
 
+        // addListenerOnButton();
+        // increase/decrease button
+
+        viewModel.savePlayer();
+    }
+
+    // pilot points
+    public void increasePointPilot(View view) {
+        pointValPilot++;
+        displayPilotPoints(pointValPilot);
+    }
+
+    public void decreasePointPilot(View view) {
+        pointValPilot--;
+        displayPilotPoints(pointValPilot);
+    }
+
+    public void displayPilotPoints(int value) {
+        TextView pointDisplay = (TextView) findViewById(R.id.pilot_points);
+        pointDisplay.setText("" + value);
+    }
+
+    // fighter points
+    public void increasePointFighter(View view) {
+        pointValFighter++;
+        displayFighterPoints(pointValFighter);
+    }
+
+    public void decreasePointFighter(View view) {
+        pointValFighter--;
+        displayFighterPoints(pointValFighter);
+    }
+
+    public void displayFighterPoints(int value) {
+        TextView pointDisplay = (TextView) findViewById(R.id.fighter_points);
+        pointDisplay.setText("" + value);
+    }
+
+    // trader points
+    public void increasePointTrader(View view) {
+        pointValTrader++;
+        displayTraderPoints(pointValTrader);
+    }
+
+    public void decreasePointTrader(View view) {
+        pointValTrader--;
+        displayTraderPoints(pointValTrader);
+    }
+
+    public void displayTraderPoints(int value) {
+        TextView pointDisplay = (TextView) findViewById(R.id.trader_points);
+        pointDisplay.setText("" + value);
+    }
+
+    // engineer points
+    public void increasePointEngineer(View view) {
+        pointValEngineer++;
+        displayEngineerPoints(pointValEngineer);
+    }
+
+    public void decreasePointEngineer(View view) {
+        pointValEngineer--;
+        displayEngineerPoints(pointValEngineer);
+    }
+
+    public void displayEngineerPoints(int value) {
+        TextView pointDisplay = (TextView) findViewById(R.id.engineer_points);
+        pointDisplay.setText("" + value);
+    }
+
+    private void savePlayer() {
         // get int value
         int pointsPilotInt = Integer.parseInt(pointsPilot.getText().toString());
         int pointsFighterInt = Integer.parseInt(pointsFighter.getText().toString());
         int pointsTraderInt = Integer.parseInt(pointsTrader.getText().toString());
         int pointsEngineerInt = Integer.parseInt(pointsEngineer.getText().toString());
 
-        // create new player
-        // need to connect to viewModel
-        player = new Player(nameField.getText().toString(), pointsPilotInt,
-                pointsFighterInt, pointsTraderInt, pointsEngineerInt);
-
+        // set attributes ?
+        player.setName(nameField.getText().toString());
+        player.setPilot(pointsPilotInt);
+        player.setFighter(pointsFighterInt);
+        player.setTrader(pointsTraderInt);
+        player.setEngineer(pointsEngineerInt);
     }
 }
