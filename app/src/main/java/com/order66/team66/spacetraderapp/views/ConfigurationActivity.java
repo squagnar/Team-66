889bf8bb23c8f7a6568333d2f384fb7dab36682a
maryjiang <maryjiang@gatech.edu>
2019-02-18 18:05:41 -0500

    added javadocs and edited onAddPressed()

--- app/src/main/java/com/order66/team66/spacetraderapp/views/ConfigurationActivity.java
+++ app/src/main/java/com/order66/team66/spacetraderapp/views/ConfigurationActivity.java
@@ -10,13 +10,24 @@ import com.order66.team66.spacetraderapp.models.Difficulty;
 import com.order66.team66.spacetraderapp.models.Player;
 import com.order66.team66.spacetraderapp.viewmodels.ConfigurationViewModel;
 
+/**
+ * Class for adding a new player
+ */
 public class ConfigurationActivity extends AppCompatActivity {
+    /**
+     * The view model
+     */
     private ConfigurationViewModel viewModel;
 
+    /**
+     * Data for player and difficulty being edited
+     */
     private Player player;
     private Difficulty difficulty;
 
-    // Widgets to get info
+    /**
+     * Widgets to get info from
+      */
     private EditText nameField;
     private TextView pointsFighter;
     private TextView pointsTrader;
@@ -24,6 +35,9 @@ public class ConfigurationActivity extends AppCompatActivity {
     private TextView pointsEngineer;
     private Spinner difficultySpinner;
 
+    /**
+     * Initial point values for Pilot, Fighter, Trader, Engineer, and total remaining points
+     */
     int pointValPilot = 0;
     int pointValFighter = 0;
     int pointValTrader = 0;
@@ -42,16 +56,19 @@ public class ConfigurationActivity extends AppCompatActivity {
         pointsTrader = findViewById(R.id.trader_points);
         pointsEngineer = findViewById(R.id.engineer_points);
 
+        // bind with difficulty spinner
+        // put enums from Difficulty into spinner
         difficultySpinner = findViewById(R.id.difficulty_spinner);
         ArrayAdapter<Difficulty> difficultyAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, Difficulty.values());
         difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         difficultySpinner.setAdapter(difficultyAdapter);
-
-        //viewModel.savePlayer();
     }
 
-    // pilot points
+    /**
+     * Increases pilot points and decreases total remaining points when + button is pressed
+      * @param view button that was pressed
+     */
     public void increasePointPilot(View view) {
         if (pointValPilot >= 0 && pointValPilot <= 16 && pointValTotal > 0 && pointValTotal <= 16) {
             pointValPilot++;
@@ -61,6 +78,10 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Decreases pilot points and increases total remaining points when - is pressed
+     * @param view the button that was pressed
+     */
     public void decreasePointPilot(View view) {
         if (pointValPilot > 0 && pointValPilot <= 16 && pointValTotal >= 0 && pointValTotal < 16) {
             pointValPilot--;
@@ -70,12 +91,19 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Displays current pilot points
+     * @param value current pilot points
+     */
     public void displayPilotPoints(int value) {
         TextView pointDisplay = (TextView) findViewById(R.id.pilot_points);
         pointDisplay.setText("" + value);
     }
 
-    // fighter points
+    /**
+     * Increases fighter points and decreases total remaining points when + button is pressed
+     * @param view button that was pressed
+     */
     public void increasePointFighter(View view) {
         if (pointValFighter >= 0 && pointValFighter <= 16 && pointValTotal > 0 && pointValTotal <= 16) {
             pointValFighter++;
@@ -85,6 +113,10 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Decreases fighter points and increases total remaining points when - is pressed
+     * @param view the button that was pressed
+     */
     public void decreasePointFighter(View view) {
         if (pointValFighter > 0 && pointValFighter <= 16 && pointValTotal >= 0 && pointValTotal < 16) {
             pointValFighter--;
@@ -94,12 +126,19 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Displays current fighter points
+     * @param value current fighter points
+     */
     public void displayFighterPoints(int value) {
         TextView pointDisplay = (TextView) findViewById(R.id.fighter_points);
         pointDisplay.setText("" + value);
     }
 
-    // trader points
+    /**
+     * Increases trader points and decreases total remaining points when + button is pressed
+     * @param view button that was pressed
+     */
     public void increasePointTrader(View view) {
         if (pointValTrader >= 0 && pointValTrader <= 16 && pointValTotal > 0 && pointValTotal <= 16) {
             pointValTrader++;
@@ -109,6 +148,10 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Decreases trader points and increases total remaining points when - is pressed
+     * @param view the button that was pressed
+     */
     public void decreasePointTrader(View view) {
         if (pointValTrader > 0 && pointValTrader <= 16 && pointValTotal >= 0 && pointValTotal < 16) {
             pointValTrader--;
@@ -118,12 +161,19 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Displays current trader points
+     * @param value current trader points
+     */
     public void displayTraderPoints(int value) {
         TextView pointDisplay = (TextView) findViewById(R.id.trader_points);
         pointDisplay.setText("" + value);
     }
 
-    // engineer points
+    /**
+     * Increases engineer points and decreases total remaining points when + button is pressed
+     * @param view button that was pressed
+     */
     public void increasePointEngineer(View view) {
         if (pointValEngineer >= 0 && pointValEngineer <= 16 && pointValTotal > 0 && pointValTotal <= 16) {
             pointValEngineer++;
@@ -133,6 +183,10 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Decreases engineer points and increases total remaining points when - is pressed
+     * @param view the button that was pressed
+     */
     public void decreasePointEngineer(View view) {
         if (pointValEngineer > 0 && pointValEngineer <= 16 && pointValTotal >= 0 && pointValTotal < 16) {
             pointValEngineer--;
@@ -142,28 +196,34 @@ public class ConfigurationActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Displays current engineer points
+     * @param value current engineer points
+     */
     public void displayEngineerPoints(int value) {
         TextView pointDisplay = (TextView) findViewById(R.id.engineer_points);
         pointDisplay.setText("" + value);
     }
 
+    /**
+     * Displays total points remaining
+     * @param value current total points remaining
+     */
     public void displayPointsRemaining(int value) {
         TextView pointDisplay = (TextView) findViewById(R.id.points_remaining);
         pointDisplay.setText("" + value);
     }
-    public void onAddPressed() {
+
+    /**
+     * Button handler for add new player button
+     * @param view the button that was pressed
+     */
+    public void onAddPressed(View view) {
         // get int value
         int pointsPilotInt = Integer.parseInt(pointsPilot.getText().toString());
         int pointsFighterInt = Integer.parseInt(pointsFighter.getText().toString());
         int pointsTraderInt = Integer.parseInt(pointsTrader.getText().toString());
         int pointsEngineerInt = Integer.parseInt(pointsEngineer.getText().toString());
-
-        // set attributes
-//        player.setName(nameField.getText().toString());
-//        player.setPilot(pointsPilotInt);
-//        player.setFighter(pointsFighterInt);
-//        player.setTrader(pointsTraderInt);
-//        player.setEngineer(pointsEngineerInt);
 
         difficulty.setDifficulty((String) difficultySpinner.getSelectedItem());
 
