af4d37ff2d58693a41a2bc2cffa00fc2feaa2d90
lfischer27 <laurenfischer@gatech.edu>
2019-03-11 14:12:53 -0400

    Market View Model

---
+++ app/src/main/java/com/order66/team66/spacetraderapp/viewmodels/MarketViewModel.java
@@ -0,0 +1,22 @@
+package com.order66.team66.spacetraderapp.viewmodels;
+import android.arch.lifecycle.ViewModel;
+import com.order66.team66.spacetraderapp.models.Player;
+import com.order66.team66.spacetraderapp.models.Market;
+import com.order66.team66.spacetraderapp.models.Planet;
+
+import java.util.List;
+
+public class MarketViewModel extends ViewModel {
+        private Player player;
+        private Market market;
+        private Planet planet;
+    market = planet.getMarket();
+
+    public Planet getPlanet() {
+        return planet;
+    }
+    public Market getMarket() {
+        return market;
+    }
+    public Player getPlayer() { return player;}
+    }
