68e2f522480f37b65be2ab367eb9fed219a7141b
squagnar <chrispcarlson@gmail.com>
2019-03-11 14:15:47 -0400

    MarketViewModel code style update

--- app/src/main/java/com/order66/team66/spacetraderapp/viewmodels/MarketViewModel.java
+++ app/src/main/java/com/order66/team66/spacetraderapp/viewmodels/MarketViewModel.java
@@ -7,16 +7,24 @@ import com.order66.team66.spacetraderapp.models.Planet;
 import java.util.List;
 
 public class MarketViewModel extends ViewModel {
-        private Player player;
-        private Market market;
-        private Planet planet;
-    market = planet.getMarket();
+
+    private Player player;
+    private Market market;
+    private Planet planet;
+
+    public MarketViewModel(Player player, Planet planet) {
+        this.planet = planet;
+        this.player = player;
+        market = this.planet.getMarket();
+    }
 
     public Planet getPlanet() {
         return planet;
     }
+
     public Market getMarket() {
         return market;
     }
+
     public Player getPlayer() { return player;}
-    }
+}
